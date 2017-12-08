bd5e07e5724bea0acd5da047e5556bce1aa210c3
Beojan Stanislaus <beojan@gmail.com>
2017-12-08 15:33:11 +0100

    Initial commit

---
+++ atlas_mpl_style/__init__.py
@@ -0,0 +1,146 @@
+import matplotlib as _mpl
+import matplotlib.style as _style
+import pkg_resources as _pkg
+import atexit as _atexit
+
+_stylesheets = _pkg.resource_filename(__name__, 'stylesheets')
+_atexit.register(_pkg.cleanup_resources)
+_style.core.USER_LIBRARY_PATHS.append(_stylesheets)
+_style.core.reload_library()
+
+_EXTRA_COLORS = {
+        'paper:bg': '#eeeeee',
+        'paper:fg': '#444444',
+        'paper:bgAlt': '#e4e4e4',
+        'paper:red': '#af0000',
+        'paper:green': '#008700',
+        'paper:blue': '#005f87',
+        'paper:orange': '#d75f00',
+        'paper:pink': '#d70087',
+        'paper:purple': '#8700af',
+        'paper:lightBlue': '#0087af',
+        'paper:olive': '#5f7800',
+        'on:bg': '#1b2b34',
+        'on:fg': '#d8dee9',
+        'on:bgAlt': '#343d46',
+        'on:fgAlt': '#cdd3de',
+        'on:red': '#ec5f67',
+        'on:orange': '#f99157',
+        'on:yellow': '#fac863',
+        'on:green': '#99c794',
+        'on:cyan': '#5fb3b3',
+        'on:blue': '#6699cc',
+        'on:pink': '#c594c5',
+        'on:brown': '#ab7967'
+}
+
+_mpl.colors.EXTRA_COLORS = _EXTRA_COLORS
+_mpl.colors.colorConverter.colors.update(_EXTRA_COLORS)
+
+
+def use_atlas_style():
+    "Setup ATLAS style."
+    _style.use('atlas')
+    _mpl.rcParams['font.size'] = 16
+    _mpl.rcParams['text.latex.preamble'] = [r'\usepackage{helvet}',
+                                            r'\usepackage{sansmath}',
+                                            r'\setlength{\parindent}{0pt}'
+                                            r'\sansmath']
+
+
+def set_xlabel(label, ax=None, *args, **kwargs):
+    """
+    Set x label in ATLAS style (right aligned).
+
+    Additional parameters are passed through to `ax.set_xlabel`.
+
+    Parameters
+    ----------
+    label : str
+        Label (LaTeX permitted)
+    ax : mpl.axes.Axes, optional
+        Axes to set x label on
+    """
+    if ax is None:
+        ax = _mpl.pyplot.gca()
+    ax.set_xlabel(label, x=1.0, ha='right')
+
+
+def set_ylabel(label, ax=None, *args, **kwargs):
+    """
+    Set y label in ATLAS style (right aligned).
+
+    Additional parameters are passed through to ``ax.set_ylabel``.
+
+    Parameters
+    ----------
+    label : str
+        Label (LaTeX permitted)
+    ax : mpl.axes.Axes, optional
+        Axes to set y label on
+    """
+    if ax is None:
+        ax = _mpl.pyplot.gca()
+    ax.set_ylabel(label, y=1.0, ha='right')
+
+
+def draw_atlas_label(x, y, ax=None, status='final', simulation=False,
+                     energy=None, lumi=None, desc=None):
+    """
+    Draw ATLAS label.
+
+    Parameters
+    ----------
+    x : float
+        x position
+    y : float
+        y position
+    ax : mpl.axes.Axes, optional
+        Axes to draw label in
+    status : [ 'int' | 'wip' | 'prelim' | *'final'* ], optional
+        Approval status
+    simulation : bool (optional, default ``False``)
+        Does the plot show only MC simulation results
+    energy : str, optional
+        Centre of mass energy, including units
+    lumi : float, optional
+        Integrated luminosity in /fb
+    desc : str, optional
+        Additional description
+    """
+    if ax is None:
+        ax = _mpl.pyplot.gca()
+    sim_str = "Simulation " if simulation else ""
+
+    if status == 'final':
+        status_str = ''
+    elif status == 'int':
+        status_str = 'Internal'
+    elif status == 'wip':
+        status_str = 'Work in Progress'
+    elif status == 'prelim':
+        status_str = 'Preliminary'
+    else:
+        status_str = 'Internal (please set the status)'
+
+    show_e_nl = False
+    if energy is not None:
+        show_e_nl = True
+        energy_str = rf'$\sqrt{{s}} = $ {energy} \quad'
+    else:
+        energy_str = ''
+
+    if lumi is not None:
+        show_e_nl = True
+        lumi_str = (fr'$\displaystyle\int \textsf{{Ld}}t = {lumi:4.1f}'
+                    fr'\textsf{{fb}}^{{-1}}$')
+    else:
+        lumi_str = ''
+
+    desc_line = desc is not None
+    nl = r'\\'
+    label = (fr'\textbf{{\textit{{ATLAS}}}} {sim_str}{status_str}'
+             fr'{nl if show_e_nl else ""}'
+             fr'{energy_str}{lumi_str}{nl if desc_line else ""}'
+             fr'{desc}')
+    ax.text(x, y, label, ha='left', va='top', multialignment='left')
---
+++ setup.py
@@ -0,0 +1,81 @@
+"""A setuptools based setup module.
+
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='atlas-mpl-style',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version='0.1',
+
+    description='ATLAS style for Matplotlib 2.0+',
+    long_description=long_description,
+
+    # The project's main homepage.
+    url='https://github.com/beojan/atlas-mpl/style',
+
+    # Author details
+    author='Beojan Stanislaus',
+    author_email='beojan.stanislaus@cern.ch',
+
+    # Choose your license
+    license='GPL v3+',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Visualization',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+
+    # What does your project relate to?
+    keywords='plotting atlas matplotlib',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+
+    # Alternatively, if you want to distribute just a my_module.py, uncomment
+    # this:
+    #   py_modules=["my_module"],
+
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=['matplotlib>=2'],
+    python_requires='>=3.5',
+    include_package_data=True,
+)
