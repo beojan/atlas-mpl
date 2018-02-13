a80e23b73cff5100197931460cdaeea263db8bb1
Beojan Stanislaus <beojan@gmail.com>
2018-02-13 16:13:23 +0100

    Fix Python version

--- setup.py
+++ setup.py
@@ -56,8 +56,8 @@ setup(
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
 
     # What does your project relate to?
@@ -76,6 +76,6 @@ setup(
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=['matplotlib>=2'],
-    python_requires='>=3.5',
+    python_requires='>=3.6',
     include_package_data=True,
 )
